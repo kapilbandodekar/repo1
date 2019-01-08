4e5826a461c44b3c68e56b38537ced78527835bf
kapilbandodekar <36533520+kapilbandodekar@users.noreply.github.com>
2019-01-08 12:17:43 +0530

    Add files via upload

---
+++ ExcelPaths.java
@@ -0,0 +1,10 @@
+package com.nics.qa.util;
+
+public class ExcelPaths {
+	
+	public static String otwdetails ="H:\\Kapil\\Selenium Test Data\\OTWCreateTestdata.xlsx";
+	public static String busidata ="H:\\Kapil\\Selenium Test Data\\OTWCreateTestdata.xlsx";
+	public static String compdata ="H:\\Kapil\\Selenium Test Data\\OTWCreateTestdata.xlsx";
+	public static String mslDetails ="H:\\Kapil\\Selenium Test Data\\OTWCreateTestdata.xlsx";
+
+}
